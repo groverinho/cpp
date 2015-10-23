403e7c9f64015e82ba7755337444785a86e20cc4
Grover Irusta Miranda <grover_ariel@hotmail.com>
2015-10-22 23:54:56 -0400

    Create WizardsandDemonstration.c

---
+++ WizardsandDemonstration.c
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, x, y;
+    cin>>n>>x>>y;
+
+	    int clones = (n * y + 99) / 100 - x;
+	    if (clones < 0)
+	    {
+	        clones = 0;
+	    }
+
+    cout << clones << endl;
+    return 0;
+}
