0450391c1a4140c6c6a399f1ffb097d2d62acdc0
Grover I <grover_ariel@hotmail.com>
2020-06-18 11:07:11 -0400

    Create searchingForNessy1428.cpp

---
+++ URI/searchingForNessy1428.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     int cases;
+    cin>>cases;
+    while(cases--){
+        int n,m;
+    cin>>n>>m;
+    cout << (n/3)*(m/3) << endl;
+    }
+
+    return 0;
+}
